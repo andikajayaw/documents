1f9f7eafaa38ca14bca38fb43dff79b27802b19c
andikajayaw <andikajayawiguna@gmail.com>
2019-08-23 00:42:34 +0700

    update script

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const sendBtn = document.querySelector('sendBtn');
+const sendBtn = document.querySelector('#sendBtn');
 const messageIn = document.querySelector('#messageIn');
 const messageOut = document.querySelector('#messageOut');
+
+sendBtn.addEventListener('click', sendMsg);
+
+function sendMsg() {
+    let content = messageIn.value;
+    console.log(content);
+}
